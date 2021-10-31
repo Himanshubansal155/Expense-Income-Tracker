d067291719c33aba8144c0c72db678fb38a8e15b
Himanshu Bansal <himanshubansal155@gmail.com>
2021-10-31 17:12:07 +0530

    scrollable changes added with scrollbar UI

--- js/addData.js
+++ js/addData.js
@@ -423,9 +423,11 @@ function showData(category) {
     });
   }
   if (displayData === "") {
-    displayData =
-      "No Transaction Available " +
-      (category !== undefined ? `for ${category}` : "");
+    displayData = `<div class="text-center w-100 h-100 d-flex justify-content-center flex-column">
+      <div><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+      <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+    </svg></div>
+    No Transaction Available</div>`;
   }
   $("#homeData").html(displayData);
   $("#transactionData").html(displayData);
@@ -521,6 +523,16 @@ function progressBarWidth() {
   if (maxIndex !== 0) {
     $("#dataprogress").append(
       `<div class="moreSection">+${maxIndex} more</div>`
+    );
+  }
+
+  if (categories.length === 0) {
+    $("#dataprogress").append(
+      `<div class="text-center w-100 h-100 d-flex justify-content-center flex-column">
+      <div><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+      <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+    </svg></div>
+      plz Add Category</div>`
     );
   }
 }
